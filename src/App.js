6161745986483a19520291490a957c4d0a55fbdc
leith15 <leith.mhf@gmail.com>
2021-02-05 09:14:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// react bootstrap
+import { Button, Navbar, Form, FormControl, Nav, Col } from "react-bootstrap";
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">REACT</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-info">Search</Button>
+          </Form>
+        </Navbar>
+
+        <br />
       </header>
+      <section>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </section>
+      <footer>
+        <MDBFooter color="blue" className="font-small pt-4 mt-4">
+          <div className="footer-copyright text-center py-3">
+            <MDBContainer fluid>
+              &copy; {new Date().getFullYear()} Copyright:{" "}
+              <a href=""> Checkpoint_REACT </a>
+            </MDBContainer>
+          </div>
+        </MDBFooter>
+      </footer>
     </div>
   );
 }
